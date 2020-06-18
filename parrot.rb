be22feef1e1970f93227d2ab555ade0524c1347e
Travis Fox <travis.fox90@gmail.com>
2020-06-18 00:25:16 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def #parrot("parrot_says_string"="Squawk!")
+def #parrot(parrot_says_string="Squawk!")
   puts parrot_says_string
   return parrot_says_string
 end
